f2465e7fbe1d28c7e40c8d2990f17cb8f3a249ad
Marcelo Melo <melooliveira31@gmail.com>
2023-11-28 14:33:48 +0000

    First push

---
+++ app.js
@@ -0,0 +1,31 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv").config();
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
+// 👇 Start handling routes here
+const indexRoutes = require("./routes/index.routes");
+const projectRoutes = require("./routes/project.routes")
+const taskRoutes = require("./routes/task.routes")
+const authRoutes = require("./routes/auth.routes")
+
+app.use("/api", indexRoutes);
+app.use("/api", projectRoutes);
+app.use("/api", taskRoutes);
+app.use("/auth", authRoutes)
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
