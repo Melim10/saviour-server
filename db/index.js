708c743b743b76b130bc63fcec4a90a96fcb6387
Marcelo Melo <melooliveira31@gmail.com>
2023-11-29 10:37:18 +0000

    Atlas configuration done

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/saviourDB";
+  "mongodb+srv://ironhacker:VEvA4YsWu6L282gh@cluster.uoizsxr.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI)
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,115 +1,114 @@
-const express = require("express");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User.model');
+
+const {isAuthenticated} = require('../middleware/jwt.middleware');
+
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-const User = require("../models/User.model");
-const {isAuthenticated} = require('../middleware/jwt.middleware')
+const saltRounds = 10; 
 
-const saltRounds= 10
+// POST '/auth/signup' - Creates a new user in the database
+router.post('/signup', (req,res)=>{
+    const {email, password, name} = req.body; 
+    /* doing object destructuring is the same as: 
+    const email = req.body.email; 
+    const password = req.body.password; 
+    const name = req.body.name; 
+    */
 
-router.post("/signup", (req, res) => {
-  const { email, password, name } = req.body;
-  if(email === '' || password === '' || name === ''){
-    res.status(400).json({message: "Provide email, password and name"})
-    return;
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
-
-  if(!emailRegex.test(email)){
-    res.status(400).json({message: 'Provide a valid email.'})
-    return
-  }
-
-  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if(!passwordRegex.test(password)){
-    res.status(400).json({message: 'Password must have at least 6 characters and contain 1 lowercase letter and 1 uppercase letter.'})
-    return
-  }
-  User.findOne({ email })
-  .then((foundUser) => {
-    // If the user with the same email already exists, send an error response
-    if (foundUser) {
-      res.status(400).json({ message: "User already exists." });
-      return;
+    /*  "What if I don't have all the required fields with information?"  */
+    if(email === '' || password === '' || name === ''){
+        res.status(400).json({message: "Provide email, password and name."})
+        return; // -> return will stop the code. 
     }
 
-    // If the email is unique, proceed to hash the password
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+    /* "What if I have an e-mail, but it's not a real e-mail? E.g.: user.com" */
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
+    if(!emailRegex.test(email)){
+        res.status(400).json({message: 'Provide a valid e-mail.'})
+        return; 
+    }
 
-    // Create a new user in the database
-    // We return a pending promise, which allows us to chain another `then` 
-    return User.create({ email, password: hashedPassword, name });
-  })
-  .then((createdUser) => {
-    // Deconstruct the newly created user object to omit the password
-    // We should never expose passwords publicly
-    const { email, name, _id } = createdUser;
-  
-    // Create a new object that doesn't expose the password
-    const user = { email, name, _id };
+    /* "What if I want password validation? In order to avoid pass like: "123" */
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if(!passwordRegex.test(password)){
+        res.status(400).json({message: 'Password must have at least 6 characters and contain 1 lowercase letter, 1 uppercase letter, 1 number'}); 
+        return; 
+    }
 
-    // Send a json response containing the user object
-    res.status(201).json({ user: user });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ message: "Internal Server Error" })
-  });
+    /* "What if a user already exists?" */
+    User.findOne({email})
+        .then((foundUser)=>{
+            if(foundUser){
+                res.status(400).json({message: 'User already exists'});
+                return;
+            }
+
+            /* Encrypt a Password */
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hashedPassword = bcrypt.hashSync(password, salt);
+
+            return User.create({email, password: hashedPassword, name});
+        }).then((createdUser)=>{
+            const {email, name, _id} = createdUser;
+
+            const user = {email, name, _id};
+
+            res.status(201).json({user});
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.status(500).json({message: 'Internal Server Error'})
+        })
 });
 
-// POST  /auth/login - Verifies email and password and returns a JWT
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-   
-    // Check if email or password are provided as empty string 
-    if (email === '' || password === '') {
-      res.status(400).json({ message: "Provide email and password." });
-      return;
+// POST '/auth/login' - Verifies email and password and returns a JWT
+router.post('/login', (req, res)=>{
+    const {email, password} = req.body; 
+
+    /* What if email and password were left blank? */
+    if(email === '' || password === ''){
+        res.status(400).json({message: 'Provide email and password.'}); 
+        return;
     }
-   
-    // Check the users collection if a user with the same email exists
-    User.findOne({ email })
-      .then((foundUser) => {
-      
-        if (!foundUser) {
-          // If the user is not found, send an error response
-          res.status(401).json({ message: "User not found." })
-          return;
-        }
-   
-        // Compare the provided password with the one saved in the database
-        const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
-   
-        if (passwordCorrect) {
-          // Deconstruct the user object to omit the password
-          const { _id, email, name } = foundUser;
-          
-          // Create an object that will be set as the token payload
-          const payload = { _id, email, name };
-   
-          // Create and sign the token
-          const authToken = jwt.sign( 
-            payload,
-            process.env.TOKEN_SECRET,
-            { algorithm: 'HS256', expiresIn: "6h" }
-          );
-   
-          // Send the token as the response
-          res.status(200).json({ authToken: authToken });
-        }
-        else {
-          res.status(400).json({ message: "Unable to authenticate the user" });
-        }
-   
-      })
-      .catch(() => res.status(500).json({ message: "Internal Server Error" }));
-  });
 
-  router.get('/verify', isAuthenticated, (req,res)=>{
-    res.status(200).json(req.payload)
-  })
+    User.findOne({email})
+        .then((foundUser)=>{
+            /* What if the user was not found? */
+            if(!foundUser){
+                res.status(400).json({message: 'User not found'});
+                return; 
+            }
 
-module.exports = router;
+            /* What if the password is not correct? */
+            const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+            if(passwordCorrect){
+                const {_id, email, name} = foundUser; 
+
+                const payload = {_id, email, name};
+
+                const authToken = jwt.sign(
+                    payload, process.env.TOKEN_SECRET, {algorithm: 'HS256', expiresIn: '6h'}
+                )
+                return res.status(200).json({authToken: authToken});
+            }
+            /* What if the password is not correct? */
+            else{
+                return res.status(400).json({message: 'Password not found'});
+            }
+        })
+        .catch(()=> res.status(500).json({message: 'Internal server error.'}))
+}); 
+
+// GET '/auth/verify' - Used to verify JWT 
+router.get('/verify', isAuthenticated, (req,res)=>{
+    res.status(200).json(req.payload);
+})
+
+
+
+module.exports = router; 
